37d2b0ff6b0da84a964fce0e0ea75ae96bea381a
luk <schwarzblade@gmail.com>
2020-07-13 20:07:29 +0200

    components

---
+++ src/components/Room.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Link from "react-router-dom";
+
+export default function Room({ room }) {
+  return <div>Hello from room</div>;
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 import Services from "../components/Services";
+import FeaturedRooms from "../components/FeaturedRooms";
 
 export const Home = () => {
   return (
@@ -14,7 +15,8 @@ export const Home = () => {
           </Link>
         </Banner>
       </Hero>
-      <Services></Services>
+      <Services />
+      <FeaturedRooms />
     </>
   );
 };
