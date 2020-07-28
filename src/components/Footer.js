7b0c36e3a3fbedd14099ba24752debb458293d99
luk <schwarzblade@gmail.com>
2020-07-28 10:08:11 +0200

    footer info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ import logo from "../images/logo.svg";
 import { FaFacebookSquare } from "react-icons/fa";
 import { BsFillBootstrapFill } from "react-icons/bs";
 import bookig from "../images/booking5.png";
+
 export default function Footer() {
   return (
     <div className="footer">
@@ -11,7 +12,28 @@ export default function Footer() {
         <Link to="/">
           <img src={logo} alt="SŁONECZKO-Jak u Mamy" />
         </Link>
+        <div className="footerInfo">
+          <ul className="owner">
+            <li>Lucyna Zając</li>
+            <li>tel.(018)331-01-99</li>
+            <li>kom.694-842-268</li>
+            <li>ul.Stawowa 35</li>
+            <li>34 -730 Mszana Dolna</li>
+          </ul>
+          <ul className="footerNav">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/rooms">Pokoje</Link>
+            </li>
+            <li>
+              <Link to="/contact">Kontakt</Link>
+            </li>
+          </ul>
+        </div>
       </div>
+
       <div className="socialMedia">
         <h3>Szukaj nas na:</h3>
         <span>
