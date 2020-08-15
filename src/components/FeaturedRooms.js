d4c5f385dc6b33eaff46583c3a8ab3a82fd29736
luk <schwarzblade@gmail.com>
2020-08-15 14:38:10 +0200

    animate featured roomms

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -3,8 +3,9 @@ import { RoomContext } from "../context";
 import Loading from "./Loading";
 import Room from "./Room";
 import Title from "./Title";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+// import gsap from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
+import observers from "observers";
 
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
@@ -12,26 +13,29 @@ export default class FeaturedRooms extends Component {
   componentDidMount() {
     const featured1 = document.getElementsByClassName("featured-rooms-center");
 
-    gsap.registerPlugin(ScrollTrigger);
+    const faders = document.querySelectorAll(".fade-in");
+    const appearOptions = {
+      threshold: 0.3,
+      rootMargin: "0px 0px 0px 0px",
+    };
+    const appearOnScroll = new IntersectionObserver(function (
+      entries,
+      appearOnScroll
+    ) {
+      entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+          entry.target.classList.remove("appear");
+        } else {
+          entry.target.classList.add("appear");
+          // appearOnScroll.unobserve(entry.target);
+        }
+      });
+    },
+    appearOptions);
 
-    gsap.fromTo(
-      featured1,
-      {
-        y: "+=100",
-        opacity: 0,
-      },
-      {
-        y: 0,
-        opacity: 1,
-        stagger: 0.2,
-        duration: 1,
-        scrollTrigger: {
-          trigger: ".featured-rooms",
-          start: "top 20%",
-          // markers: true,
-        },
-      }
-    );
+    faders.forEach((fader) => {
+      appearOnScroll.observe(fader);
+    });
   }
 
   render() {
@@ -42,7 +46,7 @@ export default class FeaturedRooms extends Component {
     return (
       <section className="featured-rooms">
         <Title title="Pokoje" />
-        <div className="featured-rooms-center">
+        <div className="featured-rooms-center fade-in">
           {loading ? <Loading /> : rooms}
         </div>
       </section>
