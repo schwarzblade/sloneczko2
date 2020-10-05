7ac69086c3f29655cbc57ade71f8af0ff773ae0d
luk <schwarzblade@gmail.com>
2020-10-05 15:19:07 +0200

    offert css

--- src/pages/NewYear.js
+++ src/pages/NewYear.js
@@ -1,91 +1,142 @@
 import React from "react";
+import { AiOutlineForward } from "react-icons/ai";
 
+import { AiOutlineClockCircle } from "react-icons/ai";
 export default function NewYear() {
   return (
     <div className="newyearsoffer">
       {" "}
-      <h2>Sylwester 2020/2021</h2>
+      <h2 className="newyear-header">Sylwester 2020/2021</h2>
       <h2>
-        <p>Termin: 28.12.2020 r. - 1.01.2021 r.</p>
+        <p className="newyear-header"> Termin: 28.12.2020 r. - 1.01.2021 r.</p>
       </h2>
-      <h2>Jesteś singlem, singielką?</h2>
-      <p>
-        {" "}
-        Wszyscy znajomi sparowani i z dziećmi? Nie masz kompletnie pomysłu co
-        począć ze sobą w Sylwestra? Żaden problem. Po to jesteśmy. Zapraszamy na
-        kilkudniowego Sylwestra tylko dla singli w góry, żadnych par i dzieci.
-        Będzie bardzo integracyjnie i wesoło. Na pewno tego wypadu długo nie
-        zapomnisz i wrócisz do domu z nowymi znajomościami a może i miłością
-        swojego życia. Spotykamy się w Beskidzie Wyspowym w miejscowości Mszana
-        Dolna. Zapraszamy single w wieku 35-55 lat. To świetna okazja, by nie
-        spędzić tej nocy samotnie a przy okazji można poznać fajnych ludzi i
-        spędzić z nimi ciekawie te kilka wolnych dni. Proponujemy, o ile dopisze
-        śnieg, połączyć ten wypad z nartami, gdyż 10 km od Mszany Dolnej mieści
-        się świetny stok w Kasinie Wielkiej, miejscowości, z której pochodzi
-        Justyna Kowalczyk. Dla osób, które chciałyby postawić pierwsze kroki na
-        nartach oferujemy nie byle gratkę bo bezpłatne lekcje na nartach. Lekcje
-        na luzie i profesjonalnie. W innych ośrodkach taka lekcja to minimum 100
-        zł za godzinę. Stok liczy 1500 m, oślą łączkę do nauki, nowoczesną
-        6-osobową kanapę i dobrą karczmę. Na miejscu znajduje się również
-        wypożyczalnia sprzętu narciarskiego. Dla osób, których zupełnie nie
-        pociąga jazda na nartach oferujemy wypad w góry po pobliskim, pięknym
-        Beskidzie Wyspowym, wyjazd do największych i najnowocześniejszych w
-        Polsce term w Chochołowie oraz wypad do Zakopanego na wycieczkę Doliną
-        Białego. Atrakcji nie zabraknie. Wieczory zagospodarujemy imprezkami
-        integracyjnymi i ogniskiem z kiełbaskami i grzańcem. Jeśli będzie dużo
-        śniegu być może uda się zorganizować kulig. Wszystko zależy od pogody.
-      </p>
-      <p>
-        <ul>
-          Program:
+      <div className="center">
+        <h2>Jesteś singlem, singielką?</h2>
+        <p>
+          {" "}
+          Wszyscy znajomi sparowani i z dziećmi? Nie masz kompletnie pomysłu co
+          począć ze sobą w Sylwestra? Żaden problem. Po to jesteśmy. Zapraszamy
+          na kilkudniowego Sylwestra tylko dla singli w góry, żadnych par i
+          dzieci. Będzie bardzo integracyjnie i wesoło. Na pewno tego wypadu
+          długo nie zapomnisz i wrócisz do domu z nowymi znajomościami a może i
+          miłością swojego życia. Spotykamy się w Beskidzie Wyspowym w
+          miejscowości Mszana Dolna. Zapraszamy single w wieku 35-55 lat. To
+          świetna okazja, by nie spędzić tej nocy samotnie a przy okazji można
+          poznać fajnych ludzi i spędzić z nimi ciekawie te kilka wolnych dni.
+          Proponujemy, o ile dopisze śnieg, połączyć ten wypad z nartami, gdyż
+          10 km od Mszany Dolnej mieści się świetny stok w Kasinie Wielkiej,
+          miejscowości, z której pochodzi Justyna Kowalczyk. Dla osób, które
+          chciałyby postawić pierwsze kroki na nartach oferujemy nie byle gratkę
+          bo bezpłatne lekcje na nartach. Lekcje na luzie i profesjonalnie. W
+          innych ośrodkach taka lekcja to minimum 100 zł za godzinę. Stok liczy
+          1500 m, oślą łączkę do nauki, nowoczesną 6-osobową kanapę i dobrą
+          karczmę. Na miejscu znajduje się również wypożyczalnia sprzętu
+          narciarskiego. Dla osób, których zupełnie nie pociąga jazda na nartach
+          oferujemy wypad w góry po pobliskim, pięknym Beskidzie Wyspowym,
+          wyjazd do największych i najnowocześniejszych w Polsce term w
+          Chochołowie oraz wypad do Zakopanego na wycieczkę Doliną Białego.
+          Atrakcji nie zabraknie. Wieczory zagospodarujemy imprezkami
+          integracyjnymi i ogniskiem z kiełbaskami i grzańcem. Jeśli będzie dużo
+          śniegu być może uda się zorganizować kulig. Wszystko zależy od pogody.
+        </p>{" "}
+        <span className="newyear-center">Program:</span>
+        <ul className="newyear-list">
           <li>
-            28.12.2019 16.00-18.00 - Zakwaterowanie (pokoje 2,3-4 osobowe) 18.00
-            - Obiadokolacja 19.00 - Impreza integracyjna z tańcami
+            <AiOutlineForward />
+            <span className="date">28.12.2020</span> <br />
+            <AiOutlineClockCircle />
+            16.00-18.00 - Zakwaterowanie (pokoje 2,3-4 osobowe) <br />{" "}
+            <AiOutlineClockCircle />
+            18.00 - Obiadokolacja
+            <br /> <AiOutlineClockCircle />
+            19.00 - Impreza integracyjna z tańcami
           </li>
           <li>
-            29.12.2019 9.00 - Śniadanie 10.00 - Narciarze na stok, natomiast
-            grupa piesza na wędrówkę po górach. 16.00 - Obiad 16.00-19.00 –
-            Odpoczynek, kawa z ciastem, gry integracyjne na świetlicy
-            (kalambury, planszówki, piłkarzyki itp...) 19.00 – Kolacja 20.00 –
-            Imprezka
+            <AiOutlineForward />
+            <span className="date">29.12.2020</span>
+            <br /> <AiOutlineClockCircle />
+            9.00 - Śniadanie <br /> <AiOutlineClockCircle />
+            10.00 - Narciarze na stok, natomiast grupa piesza na wędrówkę po
+            górach.
+            <br />
+            <AiOutlineClockCircle />
+            16.00 - Obiad <br />
+            <AiOutlineClockCircle />
+            16.00-19.00 – Odpoczynek, kawa z ciastem, gry integracyjne na
+            świetlicy (kalambury, planszówki, piłkarzyki itp...) <br />
+            <AiOutlineClockCircle />
+            19.00 – Kolacja <br />
+            <AiOutlineClockCircle />
+            20.00 – Imprezka
           </li>
           <li>
-            30.12.2019 9.00 - Śniadanie 10.00 – Narciarze stok, reszta wypad na
-            Termy Chochołowskie 16.00 – Obiad 16.00 - 18.00 Czas dla siebie
+            <AiOutlineForward />
+            <span className="date">30.12.2020</span> <br />
+            <AiOutlineClockCircle />
+            9.00 - Śniadanie
+            <br /> <AiOutlineClockCircle />
+            10.00 – Narciarze stok, reszta wypad na Termy Chochołowskie <br />
+            <AiOutlineClockCircle />
+            16.00 – Obiad
+            <br />
+            <AiOutlineClockCircle />
+            16.00 - 18.00 Czas dla siebie
+            <br /> <AiOutlineClockCircle />
             18.00 do wieczora – Ognisko z kiełbaskami i grzańcem.
           </li>
           <li>
-            31.12.2019 9.00 - Śniadanie 10.00 – Narciarze wyjazd na stok,
-            pozostali wypad do Zakopanego na spacer przepiękną Doliną Białego
-            16.00 – Obiad 17.00 – Czas własny i przygotowania do nocy
-            Sylwestrowej 19.00 – Uroczysta kolacja i zabawa do rana
+            <AiOutlineForward />
+            <span className="date">31.12.2020</span>
+            <br /> <AiOutlineClockCircle />
+            9.00 - Śniadanie
+            <br /> <AiOutlineClockCircle />
+            10.00 – Narciarze wyjazd na stok, pozostali wypad do Zakopanego na
+            spacer przepiękną Doliną Białego
+            <br /> <AiOutlineClockCircle />
+            16.00 – Obiad <br />
+            <AiOutlineClockCircle />
+            17.00 – Czas własny i przygotowania do nocy Sylwestrowej <br />
+            <AiOutlineClockCircle />
+            19.00 – Uroczysta kolacja i zabawa do rana
           </li>
           <li>
-            1.01.2020 10.00-11.00 - Śniadanie 12.00 - Spacer na otrzeźwienie
-            umysłu a dla chętnych powrót do domu 15.00 - Obiad 17.00 -
-            Wykwaterowanie.
+            <AiOutlineForward />
+            <span className="date">1.01.2021</span>
+            <br /> <AiOutlineClockCircle />
+            10.00-11.00 - Śniadanie
+            <br />
+            <AiOutlineClockCircle />
+            12.00 - Spacer na otrzeźwienie umysłu a dla chętnych powrót do domu{" "}
+            <br />
+            <AiOutlineClockCircle />
+            15.00 - Obiad <br />
+            <AiOutlineClockCircle />
+            17.00 - Wykwaterowanie.
           </li>
         </ul>
-        Za dodatkową opłatą możliwość pozostania do 2 stycznia. Cena: 850 zł
-        (cena przy wpłacie zaliczki 300 zł do końca listopada) 900 zł (cena przy
-        wpłacie zaliczki po 1 grudnia) Sylwester odbędzie się w Pensjonacie
-        Słoneczko w Mszanie Dolnej, znany z doskonałej atmosfery i przepysznego
-        domowego jedzenia. Miejsc mamy 56. Gwarantujemy, że połowę przeznaczamy
-        dla mężczyzn a połowę dla kobiet. Zgłoszenia pod numerem telefonu: 601
-        279 579 Zaliczkę 300 zł należy wpłacać na konto pensjonatu zaraz po
-        zgłoszeniu. Podczas nocy Sylwestrowej serwowane będą pyszne ciepłe
-        posiłki i zimna płyta, szampan oraz ciepłe napoje. Reszta alkoholu we
-        własnym zakresie. Na wszystkie pojawiające się pytania odpowiadam pod
-        numerem telefonu 601 279 579, lub na facebooku.
-      </p>
-      <p>
-        <h3 class="bank_account">Wpłat należy dokonywać na:</h3>
-      </p>
-      <p>
-        <h3 class="bank_account">
-          PKO Bank Polski SA, nr konta: 21 1020 3453 0000 8302 0303 2273
-        </h3>
-      </p>
+        <br />
+        <p className="newyearsoffer-footer">
+          Za dodatkową opłatą możliwość pozostania do 2 stycznia. Cena: 850 zł
+          (cena przy wpłacie zaliczki 300 zł do końca listopada) 900 zł (cena
+          przy wpłacie zaliczki po 1 grudnia) Sylwester odbędzie się w
+          Pensjonacie Słoneczko w Mszanie Dolnej, znany z doskonałej atmosfery i
+          przepysznego domowego jedzenia. Miejsc mamy 56. Gwarantujemy, że
+          połowę przeznaczamy dla mężczyzn a połowę dla kobiet. Zgłoszenia pod
+          numerem telefonu: 601 279 579 Zaliczkę 300 zł należy wpłacać na konto
+          pensjonatu zaraz po zgłoszeniu. Podczas nocy Sylwestrowej serwowane
+          będą pyszne ciepłe posiłki i zimna płyta, szampan oraz ciepłe napoje.
+          Reszta alkoholu we własnym zakresie. Na wszystkie pojawiające się
+          pytania odpowiadam pod numerem telefonu 601 279 579, lub na facebooku.
+          <p>
+            <h3 className=" newyear-header">Wpłat należy dokonywać na:</h3>
+          </p>
+          <p>
+            <h3 className=" newyear-header">
+              PKO Bank Polski SA, nr konta: 21 1020 3453 0000 8302 0303 2273
+            </h3>
+          </p>
+        </p>
+      </div>
     </div>
   );
 }
